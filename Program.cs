fb6c8aabdc7738dc8b6b3457bd533737ba5c5cfa
SaadAbdullah194 <saad.abdullah.m3mk4@gmail.com>
2024-11-01 10:11:48 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            age age1 = new age();
+            Console.WriteLine(age1.calculateage(2007));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ age.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    internal class age
+    {
+        private int birthyear;
+        private static int currentyear = 2024;
+
+
+        public double  calculateage(int birthyear) 
+        {
+           
+            return currentyear - birthyear;
+        }
+    }
+}
